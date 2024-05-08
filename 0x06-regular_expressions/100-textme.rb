c5a06fd73e07a31224ced1b6d2157f2c45556891
gabrielnoah129@gmail.com <Geff115>
2024-05-08 02:50:11 +0100

    A regex expression that matches the SENDER, RECEIVER, FLAGS from a text message log

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-matches = ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\]\s*\[to:([a-zA-Z0-9+]+)\]\s*\[flags:(.+?)\])
-puts matches.map { |match| match.join(',') }
+puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\]\s*\[to:([a-zA-Z0-9+]+)\]\s*\[flags:(.+?)\]).join(',')
