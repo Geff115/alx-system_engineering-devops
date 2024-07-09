4841c16c732f1f47759fe3b794a045cbb764e090
Geff115 <gabrielnoah129@gmail.com>
2024-07-09 03:11:28 +0100

    Using Puppet to create a custom HTTP header response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,22 +5,24 @@ exec { 'update server':
   user     => 'root',
   provider => 'shell',
 }
-
+->
+# install nginx on a server
 package { 'nginx':
   ensure   => present,
   provider => 'apt'
 }
-
+->
+# custom header response(X-Served-By: hostname)
 file_line { 'add HTTP header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;'
 }
-
+->
 # start webserver service
 service { 'nginx':
   ensure  => 'running',
   enable  => true,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
