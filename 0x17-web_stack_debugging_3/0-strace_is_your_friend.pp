60e6a47ba66d5a60479f918dbd895bcd632295f0
Geff115 <gabrielnoah129@gmail.com>
2024-08-18 14:47:55 +0100

    Fix: Apache service not working, returning a status code of 500 indicating a server error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,10 @@
 # This Puppet manifest fixes the HTTP 500 status code indicating an error in the Apache server
 
-apache::mod::rewrite { 'enabled':
-  ensure => 'present',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 service { 'apache2':
   ensure => 'restarted',
-}
-
-file { '/var/www/html/index.php':
-  ensure  => 'file',
-  owner   => 'apache',
-  group   => 'apache',
-  mode    => '0644',
-  content => template('index.php.erb'),
 }
