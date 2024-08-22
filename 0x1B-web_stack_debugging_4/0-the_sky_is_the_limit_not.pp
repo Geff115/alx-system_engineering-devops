51ba55a29db8850a68aa06ea7a2992342b61b706
Geff115 <gabrielnoah129@gmail.com>
2024-08-22 23:57:22 +0100

    Fix: The error from ApacheBench indicating the limit of server requests in the nginx default file, increased ULIMIT to 4096

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# This script increases the ULIMIT for the NGINX default file to 4096
+
+exec { 'increase-file-limit-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart-nginx-service':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
