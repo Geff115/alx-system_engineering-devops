b26c75947fa0c0a73025e4a77707b1b0ed8b0a5a
Geff115 <gabrielnoah129@gmail.com>
2024-08-23 00:27:21 +0100

    Update: Changed the OS configuration so that it is possible to login with the holberton user and open a file without any error message.

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,22 @@
+# This puppet manifest changes the OS configuration so that it is possible to
+# login with the holberton user and open a file without any error message.
+
+user { 'holberton':
+  ensure     => 'present',
+  managehome => true,
+  home       => '/home/holberton',
+  shell      => '/bin/bash',
+}
+
+file { '/home/holberton':
+  ensure  => 'directory',
+  owner   => 'holberton',
+  group   => 'holberton',
+  mode    => '0700',
+  recurse => true,
+}
+
+exec { 'unlock-holberton-user':
+  command => '/usr/sbin/usermod -U holberton',
+  onlyif  => '/bin/grep -q "^holberton:" /etc/shadow && /bin/grep -q "^holberton:!!" /etc/shadow',
+}
